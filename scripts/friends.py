bbb17fc40a47b14b45b0f855dbd489e86e610700
schmidtbit <schmidtabox@gmail.com>
2018-08-24 16:27:05 -0500

    final

--- scripts/friends.py
+++ scripts/friends.py
@@ -4,30 +4,54 @@ import itertools
 
 class friends_list(MRJob):
 
-    def mapper1(self, _, line):
-        f1 = int(line.split()[0])
-        f2 = int(line.split()[1])
+    def mapper(self, _, line):
+        # turn each row into integer pairs
+        clean = [int(x) for x in line.strip().split(' ')]
+        f1 = clean[0]
+        f2 = clean[1]
         yield (f1 ,f2)
         yield (f2, f1)
 
-    def reducer1(self, key, values):
-        f_list = list(values)
-        yield (key, f_list)
+    def reducer(self, key, values):
+        # this reducer just makes a list of all the values with that same key
+        yield (key, list(values))
 
     def mapper2(self, key, f_list):
+        # make all possible friend combinations
         pairs = itertools.combinations(f_list,2)
         for i in pairs:
-                yield (i , 1)
+            yield (i , 1)
+        # reassign value to 0 if already friends
+        for f in f_list:
+            yield (key, f), 0
 
     def reducer2(self, key, values):
-        yield (key , sum(values))
+        # count how many friends each pair has in common
+        count = 0
+        for flag in values:
+            if flag == 0:
+                # if friend pair is already friends, discard
+                return
+            count+= flag
+        yield (key , count)
 
+    def mapper3(self, key, values):
+        # reformat the friend pairs and values
+        yield key[0], (key[1], values)
+        yield key[1], (key[0], values)
+
+    def reducer3(self, key, values):
+        # recommend the user to friend the user with the most connections
+        f2 = max(values, key=lambda x : x[1])[0]
+        yield key, f2
 
     def steps(self):
-        return [MRStep(mapper=self.mapper1,
-        reducer=self.reducer1),
+        return [MRStep(mapper=self.mapper,
+        reducer=self.reducer),
         MRStep(mapper=self.mapper2,
-        reducer=self.reducer2),]
+        reducer=self.reducer2),
+        MRStep(mapper=self.mapper3,
+        reducer=self.reducer3)]
 
 
 if __name__ == '__main__':
